992a5c67917df15113076e154097c679c67493d0
haongo00 <43270713+haongo00@users.noreply.github.com>
2020-02-11 16:00:34 +0700

    fix ui PW1

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'giasu',
-      home: Payments(),
+      home: PassWord1(),
     );
   }
 }

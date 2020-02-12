0b2a16e7b10a41bdece537b5191e706bc023fd90
Nguyen Duy Kien <njktherapper@gmail.com>
2020-02-12 01:08:05 -0800

    delete whole file

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:test_giasu/Open_App/PassWord2.dart';
-import 'package:test_giasu/Open_App/Waiting_screen.dart';
-import 'package:test_giasu/Open_App/PassWord1.dart';
-import 'package:test_giasu/UI_Main/Account.dart';
-import 'package:test_giasu/UI_Main/Bits_Manager.dart';
-import 'package:test_giasu/UI_Main/Bo_Loc.dart';
-import 'package:test_giasu/UI_Main/BottomNavigationBar.dart';
-import 'package:test_giasu/UI_Main/Invite_Friend.dart';
-import 'package:test_giasu/UI_Main/Lists_Class.dart';
-import 'package:test_giasu/UI_Main/Nap_Bits.dart';
-import 'package:test_giasu/UI_Main/Payments.dart';
-import 'package:test_giasu/UI_Main/PersonInfor.dart';
-import 'package:test_giasu/UI_Main/Request.dart';
-import 'package:test_giasu/UI_Main/SpecialtyInfor.dart';
-import 'package:test_giasu/UI_Main/Support_Settings.dart';
-
 import 'Open_App/SignIn_Page.dart';
 import 'Open_App/SignUp_Page.dart';
 
+
+import 'UI_Main/BottomNavigationBar.dart';
 import 'test_PW1.dart';
 
 void main() => runApp(new MyApp());
@@ -29,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'giasu',
-      home: Nap_Bits(),
+      home: BottomNavigationBar(currentIndex: 0,),
     );
   }
 }
